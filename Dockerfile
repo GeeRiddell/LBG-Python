4ec84af2e84cfe92b0caf51088e354a74b8006a6
GeeRiddell <riddellio91@gmail.com>
2023-09-29 11:24:20 +0100

    add python and pip3

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.11
 COPY . .
-RUN pip install -r requirements.txt
+RUN pip3 install -r requirements.txt
 ENV PORT = "8081"
-ENTRYPOINT ["python", "lbg.py"]
+ENTRYPOINT ["python3", "lbg.py"]
--- dockerscript.sh
+++ dockerscript.sh
@@ -7,6 +7,6 @@ echo "changes added to repo"
 docker rm -f python-app
 docker rmi seethatgee/python-app
 docker build -t seethatgee/python-app .
-docker run -d -p 80:8080  --name python-app seethatgee/python-app:latest
+docker run -d  --name python-app seethatgee/python-app:latest
 
 echo "done"
