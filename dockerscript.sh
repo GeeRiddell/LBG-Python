03f4953de11692977b32a58c6d679c4d2e354008
GeeRiddell <riddellio91@gmail.com>
2023-09-29 11:56:38 +0100

    haha

--- dockerscript.sh
+++ dockerscript.sh
@@ -9,6 +9,7 @@ echo "changes added to repo"
 docker rm -f python-app
 docker rmi seethatgee/python-app
 docker build -t seethatgee/python-app .
-docker run -d -p 80:${PORT} -e PORT=${PORT} --name python-app seethatgee/python-app:latest
+read -p "Which port? " PORT
+docker run -d -p 80:$PORT --name python-app seethatgee/python-app:latest
 
 echo "done"
