a2cdf825c08106dcd94b5ec3312053efb26f0f78
GeeRiddell <riddellio91@gmail.com>
2023-09-29 13:03:02 +0100

    update port

--- dockerscript.sh
+++ dockerscript.sh
@@ -3,7 +3,6 @@
 docker rm -f python-app
 docker rmi seethatgee/python-app
 docker build -t seethatgee/python-app .
-read -p "Which port? " PORT
-docker run -d -p 80:$PORT --name python-app seethatgee/python-app:latest
+docker run -d -p 80:${PORT} -e PORT=${PORT} --name python-app seethatgee/python-app:latest
 
 echo "done"
